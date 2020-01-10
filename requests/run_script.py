16977dca83aaf3c3d4f18f6c02262ffb668662ea
Underpig <43320976+underpig1@users.noreply.github.com>
2020-01-09 18:49:07 -1000

    Update run_script.py

--- requests/run_script.py
+++ requests/run_script.py
@@ -1,12 +1,16 @@
-def run(fileName):
-    open("./Compile_" + info["name"][:-1] + "/src/totranslate.txt", "w+").writelines([i for i in open("./" + fileName).readlines()])
+def run(fileName)
+    import ast
+    info = ast.literal_eval(str(open("./Compile_" + info["name"][:-1] + "/info.txt", "r").readlines()))
+    open("./Compile_" + info["name"][:-1] + "/src/totranslate.txt", "w+").writelines([i for i in open("./" + fileName).readlines()])
     translate()
-    open("./Compile_" + info["name"][:-1] + "/src/excute.py", "w+").writelines([i for i in open("./Compile_" + info["name"][:-1] + "/src/totranslate.txt").readlines()])
+    open("./Compile_" + info["name"][:-1] + "/src/excute.py", "w+").writelines([i for i in open("./Compile_" + info["name"][:-1] + "/src/totranslate.txt").readlines()])
     exec("./Compile_" + info["name"][:-1] + "/src/excute.py")
     os.remove("./Compile_" + info["name"][:-1] + "/src/totranslate.txt")
     os.remove("./Compile_" + info["name"][:-1] + "/src/excute.py")
 
 def translate():
+    import ast
+    info = ast.literal_eval(str(open("./Compile_" + info["name"][:-1] + "/info.txt", "r").readlines()))
     pythonInfo = {"varint": "", "varstr": "", "varfloat": "", "vararr": "", "vardict": "", "end": "", "varpar": ",", "vardictval": ":", "varparaclose": "]", "varparaopen": "[", "varinclose": "]", "varinopen": "[", "varinsplice": ":", "varsetchar": "=", "func": "def", "funcclose": "", "funcopen": "", "funcparaclose": "):", "funcparaopen": "(", "funcparapar": ",", "funcreturn": "return", "funcdefaultparasetchar": "=", "comone": "#", "commultiopen": "#", "commulticlose": "", "loopif": "if", "loopclose": "", "loopopen": "", "loopparaclose": ":", "loopparaopen": " ", "loopelseif": "elif", "loopelse": "else", "looprep": "", "loopwhile": "while", "loopwaituntil": "","loopwait": "for", "loopfor": "for", "loopistrue": "==", "loopisfalse": "=!", "loopis": "==", "loopand": "and", "loopor": "or", "loopgreat": ">", "loopless": "<", "loopgreatequal": ">=", "looplessequal": "<=", "loopadd": "+", "loopsub": "-", "loopmult": "*", "loopdiv": "/", "loopexpo": "**", "loopmod": "%", "loopinc": "+= 1", "loopdec": "-= 1", "loopaddto": "+=", "loopsubto": "-=", "loopmultto": "*=", "loopdivto": "/=", "loopbreak": "break", "loopcont": "continue", "class": "class", "classclose": "", "classopen": ":", "classinhclose": ")", "classinhopen": "(", "classself": "self", "classvar": ".", "classfunc": ".", "classinit": "__init__", "classdel": "__del__", "impre": "import ", "imclose": "", "imopen": "", "mpass": "pass", "mglob": "global", "mread": "read", "mwrite": "write"}
     with fileinput.FileInput("./Compile_" + info["name"][:-1] + "/src/totranslate.txt", inplace = True) as file:
         for line in file:
